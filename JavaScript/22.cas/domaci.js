f169c39a5408062f1774f402bedaeb642bbc3eaf
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-05-29 12:25:34 +0200

    22.cas-zadaci

--- JavaScript/22.cas/domaci.js
+++ JavaScript/22.cas/domaci.js
@@ -1,2 +1,14 @@
 // 1.	Izdvojiti N znakova sa desne strane (poslednjih N znakova) unetog stringa.
 // Npr. za string 'Pera ima devojku' i N=5, dobija se 'vojku'.
+
+function znak(string, N) {
+  return string.slice(-N);
+}
+console.log(znak("Pera ima devojku", 5));
+
+//drugi nacin
+
+function znak1(string, N) {
+  return string.substr(-1, N);
+}
+console.log(znak("Pera ima devojku", 5));
