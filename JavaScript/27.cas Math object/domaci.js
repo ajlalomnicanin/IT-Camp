40cbee5267cc5bf86e97545513aef1990a7a125e
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-06-09 01:04:11 +0200

    zadaci

--- JavaScript/27.cas Math object/domaci.js
+++ JavaScript/27.cas Math object/domaci.js
@@ -15,21 +15,48 @@ function num(broj) {
 console.log(num(4752));
 console.log(num(5247));
 
-// ili
-console.log("ili");
-
-function num1(broj) {
-  if (!isNaN(broj) || broj >= 1000 || broj <= 9999) {
-    let cifre = broj.toString().split("");
-    for (let i = 0; i < 0; i += 2) {
-      let tren = cifre[i];
-      return tren;
-    }
-  }
-}
-let rezultat = num(4752);
-console.log(rezultat);
-console.log(typeof rezultat);
-
 // 2. Write a JavaScript program to find a value which is nearest to 100 from two different given integer
 //  values.
+// Napisi JavaScript programa koji će pronaći vrednost koja je najbliža broju 100 od dva data  broja:
+function funkcija(broj1, broj2) {
+  let rezultat1 = Math.abs(broj1 - 100);
+  let rezultat2 = Math.abs(broj2 - 100);
+  if (rezultat1 === rezultat2) {
+    return `Oba broja su jednako blizu`;
+  } else if (rezultat1 < rezultat2) {
+    return `broj1 ${broj1} je blizi broju 100`;
+  } else {
+    return `broj2 ${broj2} je blizi broju 100`;
+  }
+}
+console.log(funkcija(15, 30));
+console.log(funkcija(15, 15));
+console.log(funkcija(130, 40));
+console.log(funkcija(200, 80));
+
+// function funkcija1(broj1, broj2) {
+//   let rezultat1 = broj1 - 100;
+//   let rezultat2 = broj2 - 100;
+//   if (broj1 < 100 || broj2 < 100) {
+//     if (rezultat1 === rezultat2) {
+//       return `Oba broja su jednako blizu`;
+//     } else if (rezultat1 > rezultat2) {
+//       return `broj1 ${broj1} je blizi broju 100`;
+//     } else {
+//       return `broj2 ${broj2} je blizi broju 100`;
+//     }
+//   }
+//   if (broj1 > 100 && broj2 > 100) {
+//     if (rezultat1 === rezultat2) {
+//       return `Oba broja su jednako blizu`;
+//     } else if (rezultat1 < rezultat2) {
+//       return `broj1 ${broj1} je blizi broju 100`;
+//     } else {
+//       return `broj2 ${broj2} je blizi broju 100`;
+//     }
+//   }
+// }
+// console.log(funkcija1(15, 30));
+// console.log(funkcija1(15, 15));
+// console.log(funkcija1(130, 40));
+// console.log(funkcija1(200, 80));
