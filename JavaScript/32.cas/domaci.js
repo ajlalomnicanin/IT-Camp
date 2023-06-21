a7efac12a1bdd43ab6755cb7768a6357411e7dc8
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-06-21 17:38:59 +0200

    arrays zadatak

--- JavaScript/32.cas/domaci.js
+++ JavaScript/32.cas/domaci.js
@@ -14,5 +14,20 @@ function arr_clone(array) {
 console.log(arr_clone([1, 2, 4, 0]));
 console.log(arr_clone([1, 2, [4, 0]]));
 
+console.log("3.zadatak");
 // 3. Write a JavaScript function to get the first element of an array. Passing the parameter
 // 'n' will return the first 'n' elements of the array.
+function first(array, n) {
+  if (!Array.isArray(array) || array.length === 0) {
+    return [];
+  }
+  if (n == undefined || n == null || n == 1) {
+    return array[0];
+  }
+  return array.slice(0, n);
+}
+console.log(first([7, 9, 0, -2]));
+console.log(first([], 3));
+console.log(first([7, 9, 0, -2], 3));
+console.log(first([7, 9, 0, -2], 6));
+console.log(first([7, 9, 0, -2], -3)); // 7
