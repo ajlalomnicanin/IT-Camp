ac9ac261ad924cf7ba1cc2190020bd3f39760151
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-06-30 21:02:59 +0200

    JavaScript Array Iteration metode

---
+++ JavaScript/36.cas/app.js
@@ -0,0 +1,53 @@
+// keys() metoda vraca Array Iterator Object kljuceva nekog niza.
+
+const grupa = ["Emir", "Ajsa", "Selver", "Asija"];
+//               0        1       2         3
+const kljucevi = grupa.keys();
+
+console.log(kljucevi);
+
+for (let kljuc of kljucevi) {
+  console.log(kljuc);
+}
+
+// enteries() metoda vraca Array Iterator Object key/value nekog niza.
+
+const parovi = grupa.entries();
+
+console.log(parovi);
+
+for (let par of parovi) {
+  console.log(par);
+}
+
+// callback funkcija predstavlja funkciju koju saljemo kao argument neke metode odnosno neke druge (funkcije).
+
+function glavnaFunkcija(callback) {
+  console.log("Pocetak posla glavne funkcije.");
+  callback();
+  console.log("Kraj posla glavne funkcije.");
+}
+
+function pomocna() {
+  console.log("Ovo je radnja iz callback funkcije.");
+}
+
+glavnaFunkcija(pomocna);
+
+//Zadatak. Napravi glavnu funkciju koja ima callback funkciju, i jos dva argumenta (broj). callback
+// funkcija treba da ispise prvi broj stepenovanja sa drugim
+
+function glavnaFunkcija(callback, broj1, broj2) {
+  callback(broj1, broj2);
+}
+function callback(broj1, broj2) {
+  console.log(Math.pow(broj1, broj2));
+}
+glavnaFunkcija(callback, 4, 2);
+
+// ili
+
+const main = (callback, num1, num2) => {
+  return callback(num1, num2);
+};
+console.log(main((a, b) => a ** b, 3, 2));
---
+++ JavaScript/36.cas/app1.js
@@ -0,0 +1,49 @@
+// JavaScript Array Iteration metode //
+
+// Array Itteraton metode prolaze kroz svaki element niza.
+
+// gorEach() metoda - poziva callback funkciju (koja predstavlja argument forEach() metode jednom za
+//  svaki element niza)
+
+const brojevi = [2, 4, 6, 8, 10, 12];
+brojevi.forEach((element, index, array) => {
+  console.log(index, element, array); //
+});
+// element - vrednost elementa
+// index - pozicija elementa
+// array - vraca ceo niz
+
+// callback moze imati tri argumenta :
+// 1.vrednost elementa
+// 2.index(pozicija) elementa
+// 3.ceo niz na koji se primenjuje metoda
+
+// Preko petlje napravi novi niz koji ce imati sve elemente originalnog niza
+console.log("Zadatak");
+
+function first(array) {
+  const array1 = [];
+  for (let i = 0; i < array.length; ++i) {
+    // array1[i] = array[i];
+    array1.push(array[i]);
+  }
+  return array1;
+}
+console.log(first([2, 4, 6, 8, 10, 12]));
+
+// ili
+
+function first1(array) {
+  return array.slice(0);
+}
+console.log(first1(brojevi));
+
+// ili
+const brojevi2 = [];
+brojevi.forEach((element) => {
+  brojevi2.push(element);
+});
+console.log(brojevi2);
+
+// map() metoda
+// - izvlaci novo polje sa transformisanim elemenata orginalnog polja
