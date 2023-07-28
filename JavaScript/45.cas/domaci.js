72be75ca4cc924bbacab2209aa915141651acbd6
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-07-28 23:54:42 +0200

    47 - Object built in methods

--- JavaScript/45.cas/domaci.js
+++ JavaScript/45.cas/domaci.js
@@ -63,3 +63,27 @@ const sortByTitle = (arr) => {
 };
 
 console.log(sortByTitle(library));
+
+// sortiraj po datumu
+const library2 = [
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960,
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    year: 1949,
+  },
+  {
+    title: "Brave New World",
+    author: "Aldous Huxley",
+    year: 1932,
+  },
+];
--- JavaScript/47.cas/app1.js
+++ JavaScript/47.cas/app1.js
@@ -1,32 +1,37 @@
-// Napraviti funkciju koja vraca novi niz, gde je svaki element objekat sa vrednostima title i budget.
-const movies = [
-  { title: "Inception", rating: 8, budget: "230M" },
-  { title: "Interstelar", rating: 5, budget: "200M" },
-  { title: "Godfather", rating: 9, budget: "280M" },
-  { title: "Home Alone", rating: 4, budget: "300M" },
-  { title: "Bad boys", rating: 8, budget: "180M" },
-];
-const movies2 = movies.map((movie) => {
-  return { title: movie.title, budget: movie.budget };
-});
-console.log(movies2);
-
-const movies3 = movies.map((movie) => ({
-  title: movie.title,
-  budget: movie.budget,
-}));
-console.log(movies3);
-
 // Domaci:
 // 1. Write a JavaScript function to retrieve all the values of an object's properties.
-const mojObjekat = {
-  ime: "Ajla",
-  prezime: "Lomnicanin",
-  starost: 22,
-  bojaOciju: "braon",
+
+const itCamp = {
+  duzina: "9 meseci",
+  brojTehnologija: 4,
+  prvaTehnologija: "HTML",
+  drugaTehnologija: "CSS",
+  trecaTehnologija: "JavaScript",
+  cetvrtaTehnologija: "React",
+  ocekivaniIshod: "Junior Frontend Developer",
 };
-const niz = Object.values(mojObjekat);
-console.log(niz);
+
+// for in
+
+for (let x in itCamp) {
+  console.log(x); // x je key za svaki property objekta
+  console.log(itCamp[x]); // ispisuje se value za svaki property objekta
+}
+
+// 1. nacin
+const values = (obj) => {
+  const values = [];
+  for (let key in obj) {
+    values.push(obj[key]);
+  }
+  return values;
+};
+
+console.log(values(itCamp));
+
+// 2. nacin
+const newArr = Object.values(itCamp);
+console.log(newArr);
 
 // 2. Napraviti niz objekata, gde ce svaki objekat sadrzati:
 // ime, prezime, starost, bojaOciju, bojaKose, polozenC.
@@ -76,8 +81,12 @@ const arr = [
     polozenC: false,
   },
 ];
-const arr2 = arr.filter(
-  // (st) => st.bojaOciju === "braon" && st.polozenC === false
-  (st) => st.bojaOciju === "braon" && !st.polozenC
-);
-console.log(arr2);
+
+const filterStudents = (arr) => {
+  return arr.filter(
+    // (student) => student.bojaOciju === "braon" && student.polozenC === false
+    (student) => student.bojaOciju === "braon" && !student.polozenC
+  );
+};
+
+console.log(filterStudents(arr));
