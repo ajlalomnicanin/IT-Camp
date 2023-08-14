4f514c2729c11fec0b1113581a9910a96e557da6
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-08-14 22:31:38 +0200

    fetch

--- JavaScript/50.cas/app.js
+++ JavaScript/50.cas/app.js
@@ -77,3 +77,12 @@ chain
 // Ako argument ima vise od 10 samoglasnika neka se uspesno izvrsi obecanje sa porukom "String zadovoljava uslov";
 // ako argument ima manje od 11 samoglasnika neka se reject_a obecanje uz poruku "String ne zadovoljava uslov";
 // U svakom slucaju neka stoji poruka "Funkcija je izvrsena".
+
+function samoglasnici(string){
+  return new Promise((resolve,reject)=>{
+    const samog="aeiouAEIOUU"
+    let brojSamog=0
+    for()
+
+  })
+}
